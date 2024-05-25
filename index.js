ca49f0ea052fc6a7980283916385b68924c78242
JoaoLimaV <joaolima675@outlook.com>
2024-05-24 23:39:47 -0300

    Cors add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ process.env.NODE_ENV = 'development';
 
 const express = require('express');
 const morgan = require('morgan');
+var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 10000;
@@ -9,19 +10,8 @@ const port = process.env.PORT || 10000;
 app.use(express.json({ limit: '5000kb' }));
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+// Cors 
+app.use(cors())
 
 app.use(morgan('dev'));
 
