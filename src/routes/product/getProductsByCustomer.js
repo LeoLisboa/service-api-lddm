1520403332a0192b1eb80b35ac4abf9f235989b0
LeonardoLisboa <leonardo.lisboa2700@outlook.com>
2024-06-05 15:38:08 -0300

    Fix getProductsByCustomer

--- src/routes/product/getProductsByCustomer.js
+++ src/routes/product/getProductsByCustomer.js
@@ -3,23 +3,22 @@ const router = express.Router();
 const connection = require('../../database/connection');
 const getHeaderToken = require('../getHeaderToken');
 
-// Rota para obter um produto específico
+// Rota para obter todos os produtos em que o usuário fez lances
 router.get('/', getHeaderToken, async (req, res) => {
     const userData = req.user;
     try {
-
         const query = `
         SELECT 
-        p.*, 
-        CONVERT_TZ(p.created_at, '+00:00', @@session.time_zone) AS product_created_at,
-        CONVERT_TZ(p.updated_at, '+00:00', @@session.time_zone) AS product_updated_at,
-        pb.id_user_bid, 
-        pb.price, 
-        pb.percentage, 
-        CONVERT_TZ(pb.created_at, '+00:00', @@session.time_zone) AS bid_created_at, 
-        CONVERT_TZ(pb.updated_at, '+00:00', @@session.time_zone) AS bid_updated_at, 
-        GROUP_CONCAT(pi.url) AS images, 
-        ps.status AS status_name
+            p.*, 
+            CONVERT_TZ(p.created_at, '+00:00', @@session.time_zone) AS product_created_at,
+            CONVERT_TZ(p.updated_at, '+00:00', @@session.time_zone) AS product_updated_at,
+            pb.id_user_bid, 
+            pb.price, 
+            pb.percentage, 
+            CONVERT_TZ(pb.created_at, '+00:00', @@session.time_zone) AS bid_created_at, 
+            CONVERT_TZ(pb.updated_at, '+00:00', @@session.time_zone) AS bid_updated_at, 
+            GROUP_CONCAT(pi.url) AS images, 
+            ps.status AS status_name
         FROM 
             product p 
         JOIN 
@@ -41,40 +40,49 @@ router.get('/', getHeaderToken, async (req, res) => {
 
         connection.query(query, [userData.id], (error, results) => {
             if (error) {
-                console.error('Erro ao buscar produto do banco de dados:', error);
+                console.error('Erro ao buscar produtos do banco de dados:', error);
                 return res.status(500).json({ message: "Erro no servidor" });
             }
 
             if (results.length === 0) {
-                return res.status(404).json({ message: "Produto não encontrado" });
+                return res.status(404).json({ message: "Nenhum produto encontrado para o usuário" });
             }
 
-            // Initialize the product object
-            const produto = {
-                id: results[0].id,
-                name: results[0].name,
-                desc: results[0].desc,
-                start_price: results[0].start_price,
-                final_bid_price: results[0].final_bid_price,
-                current_price: results[0].current_price,
-                created_at: results[0].created_at,
-                updated_at: results[0].updated_at,
-                status_name: results[0].status_name,
-                images: results[0].images ? results[0].images.split(',') : []
-            };
+            const produtosAgrupados = {};
 
-            produto.history_bid = results
-                .map(row => ({
+            results.forEach(row => {
+                const productId = row.id;
+
+                if (!produtosAgrupados[productId]) {
+                    produtosAgrupados[productId] = {
+                        id: row.id,
+                        name: row.name,
+                        desc: row.desc,
+                        start_price: row.start_price,
+                        final_bid_price: row.final_bid_price,
+                        current_price: row.current_price,
+                        created_at: row.created_at,
+                        updated_at: row.updated_at,
+                        status_name: row.status_name,
+                        images: row.images ? row.images.split(',') : [],
+                        history_bid: []
+                    };
+                }
+
+                produtosAgrupados[productId].history_bid.push({
                     price: row.price,
                     percentage: row.percentage,
                     bid_created_at: row.bid_created_at,
                     bid_updated_at: row.bid_updated_at
-                }))
+                });
+            });
 
-            return res.status(200).json({ produto });
+            const produtos = Object.values(produtosAgrupados);
+
+            return res.status(200).json({ produtos });
         });
     } catch (error) {
-        console.error('Erro ao buscar produto:', error);
+        console.error('Erro ao buscar produtos:', error);
         return res.status(500).json({ message: "Erro no servidor" });
     }
 });
